3f3e2b65fad9c0a1dbdf4c4cccdd0251bdccf331
Alex <proskura-s@mail.ru>
2014-09-18 01:04:52 +0300

    первый проект

---
+++ MyList/MyLibrary/MyList.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace MyLibrary
+{
+    public class MyList : IList, IEnumerator
+    {
+        //коммент
+        const int arrayZeroSize = 0;
+
+        object[] objList = new object[arrayZeroSize];
+
+        int counterOfResize = 2;
+        int counterOfAdd = 0;
+        int counterOfRealElem = 0;
+        int position = -1;
+
+        public int Add(object value)
+        {
+            try
+            {
+                objList[counterOfAdd] = value;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Resize();
+                objList[counterOfAdd] = value;
+            }
+            finally
+            {
+                counterOfAdd++;
+            }
+            return 0;
+        }
+
+        void Resize()
+        {
+            object[] tempList = new object[(int)Math.Pow(2, counterOfResize++)];
+            for (int i = 0; i < objList.Length; i++)
+            {
+                tempList[i] = objList[i];
+            }
+            objList = tempList;
+            tempList = null;
+        }
+
+        public void Clear()
+        {
+            objList = new object[arrayZeroSize];
+        }
+
+        public bool Contains(object value)
+        {
+            foreach (var item in objList)
+            {
+                try
+                {
+                    if (item.Equals(value))
+                    {
+                        return true;
+                    }
+                }
+                catch (NullReferenceException)
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public int IndexOf(object value)
+        {
+            for (int i = 0; i < objList.Length; i++)
+            {
+                try
+                {
+                    if (objList[i].Equals(value))
+                    {
+                        return i;
+                    }
+                }
+                catch (NullReferenceException)
+                {
+                    return -1;
+                }
+            }
+            return -1;
+        }
+
+        public void Insert(int index, object value)
+        {
+            try
+            {
+                objList[index] = value;
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public bool IsFixedSize
+        {
+            get { return true; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public void Remove(object value)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (objList[i].Equals(value))
+                {
+                    while (i < Count - 1)
+                    {
+                        objList[i] = objList[i + 1];
+                        i++;
+                    }
+                    break;
+                }
+            }
+            objList[Count - 1] = null;
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+            {
+                objList[i] = objList[i + 1];
+            }
+            objList[Count - 1] = null;
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                return objList[index];
+            }
+            set
+            {
+                objList[index] = value;
+            }
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            foreach (var item in objList)
+            {
+                array.SetValue(item, index++);
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                counterOfRealElem = 0;
+                foreach (var item in objList)
+                {
+                    if (item != null)
+                    {
+                        counterOfRealElem++;
+                    }
+                }
+                return counterOfRealElem;
+            }
+        }
+
+        public int Lenght
+        {
+            get
+            {
+                return objList.Length;
+            }
+        }
+
+        public bool IsSynchronized
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object SyncRoot
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return (IEnumerator)this;
+        }
+
+        public object Current
+        {
+            get { return objList[position]; }
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < objList.Length);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+}
---
+++ MyList/TestOfLibrary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLibrary;
+
+namespace TestOfLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLibrary.MyList test = new MyList();
+            test.Add(5);
+            test.Add(6);
+            test.Add(7);
+            test.Add(8);
+            test.Add(9);
+            test.Add(10);
+            test.Add(11);
+            /*
+            foreach (var item in test)
+            {
+                Console.WriteLine(item);
+            }
+             */
+            while (test.MoveNext())
+            {
+                Console.WriteLine(test.Current);
+            }
+        }
+    }
+}
