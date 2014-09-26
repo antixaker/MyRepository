74b1fb13ba23a2c5646367d0bba067b161c221a2
antixaker <proskura-s@mail.ru>
2014-09-27 02:07:06 +0300

    Долгожданный Generic

---
+++ TestGeneric/MyGeneric/Class1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGeneric
+{
+    public class MyList<T> : IList<T>
+    {
+        #region Variables
+
+        const int arrayZeroSize = 0;
+
+        T[] genericList = new T[arrayZeroSize];
+
+        int counterOfResize = 2;
+        int counterOfAdd = 0;
+        int counterOfRealElem = 0;
+        //int position = -1;
+
+        #endregion
+
+        #region Properties
+
+        public int Count
+        {
+            get
+            {
+                counterOfRealElem = 0;
+                foreach (var item in genericList)
+                {
+                    if (item != null)
+                    {
+                        counterOfRealElem++;
+                    }
+                }
+                return counterOfRealElem;
+            }
+        }
+
+        public int Lenght
+        {
+            get
+            {
+                return genericList.Length;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return genericList[index];
+            }
+            set
+            {
+                genericList[index] = value;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < genericList.Length; i++)
+            {
+                try
+                {
+                    if (genericList[i].Equals(item))
+                    {
+                        return i;
+                    }
+                }
+                catch (NullReferenceException)
+                {
+                    return -1;
+                }
+            }
+            return -1;
+        }
+
+        void Resize()
+        {
+            T[] tempList = new T[(int)Math.Pow(2, counterOfResize++)];
+            for (int i = 0; i < genericList.Length; i++)
+            {
+                tempList[i] = genericList[i];
+            }
+            genericList = tempList;
+            tempList = null;
+        }
+
+        public void Insert(int index, T item)
+        {
+            genericList[index] = item;
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+            {
+                genericList[i] = genericList[i + 1];
+            }
+            genericList[Count - 1] = default(T);
+        }
+
+        public void Add(T item)
+        {
+            if (counterOfAdd == this.Lenght)
+            {
+                Resize();
+                genericList[counterOfAdd] = item;
+                counterOfAdd++;
+            }
+            else
+            {
+                genericList[counterOfAdd] = item;
+                counterOfAdd++;
+            }
+        }
+
+        public void Clear()
+        {
+            genericList = new T[arrayZeroSize];
+        }
+
+        public bool Contains(T item)
+        {
+            foreach (T param in genericList)
+            {
+                try
+                {
+                    if (param.Equals(item))
+                    {
+                        return true;
+                    }
+                }
+                catch (NullReferenceException)
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            foreach (T item in genericList)
+            {
+                array.SetValue(item, arrayIndex++);
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            bool retvalue = false;
+
+            for (int i = 0; i < Count; i++)
+            {
+                if (genericList[i].Equals(item))
+                {
+                    retvalue = true;
+                    while (i < Count - 1)
+                    {
+                        genericList[i] = genericList[i + 1];
+                        i++;
+                    }
+                    break;
+                }
+            }
+            genericList[Count - 1] = default(T);
+            return retvalue;
+        }
+
+        #endregion
+
+        #region GetEnumerator
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Numerator temp = new Numerator(this);
+            return (IEnumerator<T>)(temp);
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return null;
+        }
+
+        class Numerator : IEnumerator<T>
+        {
+            MyList<T> exempBase;
+            int numerator_position = -1;
+
+            public Numerator(MyList<T> got)
+            {
+                exempBase = got; //Reset();
+            }
+
+            public T Current
+            {
+                get { return exempBase.genericList[numerator_position]; }
+            }
+
+            public void Dispose()
+            {
+                //exempBase.genericList = new T[arrayZeroSize];
+            }
+
+            object System.Collections.IEnumerator.Current
+            {
+                get { return exempBase.genericList[numerator_position]; }
+            }
+
+            public bool MoveNext()
+            {
+                numerator_position++;
+                return (numerator_position < exempBase.genericList.Length);
+            }
+
+            public void Reset()
+            {
+                numerator_position = -1;
+            }
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ TestGeneric/TestMyGeneric/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyGeneric;
+
+namespace TestMyGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> generic_int = new MyList<int>();
+            generic_int.Add(1);
+            generic_int.Add(2);
+            generic_int.Add(3);
+
+            MyList<float> genfloat = new MyList<float>();
+            genfloat.Add(2.45f);
+            genfloat.Add(2.46f);
+            genfloat.Add(2.47f);
+            genfloat.Add(2.48f);
+            genfloat.Add(2.49f);
+            Console.WriteLine("Друкуемо раз:");
+            foreach (var item in generic_int)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Друкуемо два:");
+            foreach (var item in genfloat)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
