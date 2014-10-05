64a263773e54bcb98a82f1c38587b65d1df52b5d
antixaker <proskura-s@mail.ru>
2014-10-06 02:35:09 +0300

    практически доделал LinkedList

--- TestOfLinkedList/MyLinkedList/MyLinkedList.cs
+++ TestOfLinkedList/MyLinkedList/MyLinkedList.cs
@@ -8,76 +8,226 @@ using System.Threading.Tasks;
 
 namespace MyLinkedList
 {
-    class Node<T>
+    public class Node<T>
     {
-        public T element;
-        public Node<T> next = null;
-        public Node<T> previous = null;
+        public T Value;
+        public Node<T> Next = null;
+        public Node<T> Prev = null;
 
         public Node()
         {
-            this.element = default(T);
+            this.Value = default(T);
+            this.Next = null;
+            this.Prev = null;
         }
 
         public Node(T _node)
         {
-            this.element = _node;
-        }
-
-        public void SetNext(T nextvalue)
-        {
-            this.next = new Node<T>(nextvalue);
-        }
-
-        public void SetPrevious(T nextvalue)
-        {
-            this.previous = new Node<T>(nextvalue);
-        }
-
-        public void SetElement(T value)
-        {
-            this.element = value;
-        }
-
-        public T GetElement(T value)
-        {
-            return this.element;
-        }
-
-
-        internal void SetPrevious(Node<T> tail)
-        {
-            throw new NotImplementedException();
-        }
-
-        internal void SetNext(Node<T> head)
-        {
-            throw new NotImplementedException();
+            this.Value = _node;
         }
     }
 
-    public class MyLinkedList<T> : ICollection<T>,IList<T>, ISerializable, IDeserializationCallback
+    public class OwnLinkedList<T> : IEnumerable<T>
     {
-       // LinkedList<T> listOfNodes = new LinkedList<T>();
-        
- 
-        int countOfNodes;
-        Node<T> head;
-        Node<T> tail;
 
-        public MyLinkedList ()
+        #region Property
+
+        Node<T> _first;
+        Node<T> _last;
+        Node<T> curr;
+
+        int size;
+
+        public Node<T> First
         {
-            head.previous=tail;
-            tail.next = head;
+            get { return _first; }
+        }
+
+        public Node<T> Last
+        {
+            get { return _last; }
+        }
+
+        public int Count
+        {
+            get { return size; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public OwnLinkedList()
+        {
+            _first = _last = curr = null;
+            size = 0;
         }
 
         public void AddFirst(T nod)
         {
             Node<T> newnod = new Node<T>(nod);
-
+            if (_first == null)
+            {
+                _first = _last = newnod;
+            }
+            else
+            {
+                newnod.Next = _first;
+                _first = newnod;
+                newnod.Next.Prev = _first;
+            }
+            size++;
         }
 
+        public void AddLast(T nod)
+        {
+            Node<T> newnod = new Node<T>(nod);
+            if (_last == null)
+            {
+                _last = _first = newnod;
+            }
+            else
+            {
+                newnod.Prev = _last;
+                _last.Next = newnod;
+                _last = newnod;
+            }
+            size++;
+        }
+
+        public Node<T> Find(T value)
+        {
+            curr = First;
+            while (curr != null)
+            {
+                if (curr.Value.Equals(value))
+                {
+                    return curr;
+                }
+                curr = curr.Next;
+            }
+            return null;
+        }
+
+        public void Clear()
+        {
+            _first = _last = curr = null;
+            size = 0;
+        }
+
+        public bool Remove(T value)
+        {
+            curr = First;
+            while (curr != null)
+            {
+                if (curr.Value.Equals(value))
+                {
+                    curr.Next.Prev = curr.Prev;
+                    curr.Prev.Next = curr.Next;
+                    size--;
+                    return true;
+                }
+                curr = curr.Next;
+            }
+            return false;
+        }
+
+        public bool RemoveFirst()
+        {
+            if (_first != null)
+            {
+                _first.Next.Prev = null;
+                _first = _first.Next;
+                size--;
+                return true;
+
+            }
+            else return false;
+        }
+
+        public bool RemoveLast()
+        {
+            if (_last != null)
+            {
+                _last.Prev.Next = null;
+                _last = _last.Prev;
+                size--;
+                return true;
+
+            }
+            else return false;
+        }
+
+        #endregion
+
+        #region Enumerator
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return (IEnumerator<T>)(new OwnLinkedList_numerator(this));
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        public class OwnLinkedList_numerator : IEnumerator<Node<T>>
+        {
 
 
+            //public object Current
+            //{
+            //    get { throw new NotImplementedException(); }
+            //}
+
+            //public bool MoveNext()
+            //{
+            //    throw new NotImplementedException();
+            //}
+
+            //public void Reset()
+            //{
+            //    throw new NotImplementedException();
+            //}
+            OwnLinkedList<T> exemp;
+            Node<T> current;
+            public OwnLinkedList_numerator(OwnLinkedList<T> got)
+            {
+                exemp = got;
+            }
+
+            public Node<T> Current
+            {
+                get { return current; }
+            }
+
+            public void Dispose()
+            {
+
+            }
+
+            object IEnumerator.Current
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public bool MoveNext()
+            {
+                if (exemp._first.Next != null)
+                {
+                    current = exemp._first = exemp._first.Next;
+                    return true;
+                }
+                return false;
+            }
+
+            public void Reset()
+            {
+                current = exemp.First;
+            }
+        }
     }
 }
--- TestOfLinkedList/TestList/Program.cs
+++ TestOfLinkedList/TestList/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyLinkedList;
 
 namespace TestList
 {
@@ -10,6 +11,20 @@ namespace TestList
     {
         static void Main(string[] args)
         {
+            OwnLinkedList<int> doublelist = new OwnLinkedList<int>();
+            doublelist.AddFirst(0);
+            doublelist.AddFirst(1);
+            doublelist.AddFirst(2);
+            doublelist.AddFirst(3);
+            doublelist.AddLast(-1);
+           // doublelist.Remove(2);
+            doublelist.RemoveFirst();
+            doublelist.RemoveLast();
+            //foreach ( Node<int> item in doublelist)
+            //{
+            //    Console.WriteLine(item.Value);  
+            //}
+
         }
     }
 }
