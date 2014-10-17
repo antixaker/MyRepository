468d86aabd4bd7be7e263f670a262964f27aca02
antixaker <proskura-s@mail.ru>
2014-10-17 17:08:56 +0300

    Первый блин

---
+++ Calculator/BinaryTree/BinaryTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+
+    public class Node
+    {
+        object Value;
+        Node left;
+        Node right;
+
+        Node(object Value)
+        {
+            this.Value = Value;
+        }
+    }
+
+    public class BinaryTree
+    {
+    }
+}
